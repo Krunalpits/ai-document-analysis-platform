cd3fcfedade5bae23d5d8c47ae57da36d87241fc
Krunal Pithadia <125281938+Krunalpits@users.noreply.github.com>
2025-11-18 18:47:42 -0800

    Nov 18 -setup db and schemas

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+export default {
+  dialect: "postgresql",                // new verson syntax
+  schema: "./src/lib/db/schema.ts",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,     // new verson syntax
+  },
+} satisfies Config;
